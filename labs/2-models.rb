24f79a66911aa7419d54e2f9a716267d68adb5dd
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-27 01:42:26 +0000

    finished models lab

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,13 +21,13 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
-salesperson1 = Salespeople.new
+salesperson1 = Salesperson.new
 salesperson1["first_name"] = "Kona"
 salesperson1["last_name"] = "Vu-Shea"
 salesperson1["email"] = "konalovessticks@gmail.com"
 salesperson1.save # actually add to table
 
-salesperson2 = Salespeople.new
+salesperson2 = Salesperson.new
 salesperson2["first_name"] = "Tofu"
 salesperson2["last_name"] = "Vu"
 salesperson2["email"] = "tofu-the-lamb@gmail.com"
@@ -35,15 +35,27 @@ salesperson2.save # actually add to table
 
 # 3. write code to display how many salespeople rows are in the database
 
-puts "There are #{Salespeople.all.count} salespeople"
+# puts "There are #{Salesperson.all.count} salespeople"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
 
+tofu = Salesperson.find_by({"first_name" => "Tofu"})
+tofu["email"] = "tofu-lamb@gmail.com" # assign it something
+tofu.save # updates
+# puts tofu["email"]
+
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+
+kona = Salesperson.find_by({"first_name" => "Kona"})
+kona_full_name = "#{kona["first_name"]} #{kona["last_name"]}"
+tofu_full_name = "#{tofu["first_name"]} #{tofu["last_name"]}"
+puts "There are #{Salesperson.all.count} salespeople:"
+puts "#{kona_full_name}"
+puts "#{tofu_full_name}"
 
 # ---------------------------------
 # Salespeople: 2
