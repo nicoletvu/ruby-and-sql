ba7f21190b60e0c64138d2a9ad7bbda1278135aa
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-27 00:59:08 +0000

    Added new rows to companies table

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -18,6 +18,24 @@ puts "There are #{Company.all.count} companies"
 
 # 2. insert new rows in companies table
 
+new_company = Company.new
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "https://www.apple.com"
+new_company.save # actually add to table
+
+new_company_2 = Company.new
+new_company_2["name"] = "Amazon"
+new_company_2["city"] = "Seattle"
+new_company_2["state"] = "WA"
+new_company_2["url"] = "https://www.amazon.com"
+new_company_2.save # actually add to table
+
+puts new_company.inspect
+puts new_company_2.inspect
+puts "There are #{Company.all.count} companies"
+
 # 3. query companies table to find all row with California company
 
 # 4. query companies table to find single row for Apple
